18e55e8377b034d19af798bad1394f2861eb6137
Eduardo Lopes B. dos Santos <eduardolbs07@gmail.com>
2025-10-07 20:39:02 -0300

    Lógica de Adição de Cartões com Item e Peso

--- js/logica.js
+++ js/logica.js
@@ -1,4 +1,66 @@
-const numeros = [3, 9, 5]
-const maior = Math.max(...numeros);
+const iptItem = document.getElementById('iptItem');
+const iptPeso = document.getElementById('iptPeso');
+const listaItens = document.getElementById('listaItens');
 
-console.log(maior)
+function listaCard() {
+    const nome = iptItem.value.trim();
+    const peso = iptPeso.value.trim();
+
+    // Evita adicionar cartões vazios
+    if (nome === '' || peso === '') {
+        alert('Por favor, preencha todos os campos!');
+        return;
+    }
+
+    const estilizacao = getComputedStyle(document.documentElement);
+    const varCores = [
+        '--azulBrains',
+        '--laranjaBrains',
+        '--brancoBrains',
+        '--pretoBrains',
+        '--roxoBrains',
+        '--rosaBrains',
+
+        '--verdeClaroFamily',
+        '--azulClaroFamily',
+        '--brancoFamily',
+        '--cinzaFamily',
+        '--cinzaFamilEscuro',
+        '--amareloFamily',
+        '--verdeNeonFamily',
+
+        '--rosaClaroLovers',
+        '--lavandaLovers',
+        '--brancoLovers',
+        '--cinzaClaroLovers',
+        '--coralLovers',
+        '--turquesaLovers',
+        '--vinhoLovers',
+        '--roxoLovers',
+
+        '--azulPersonal',
+        '--cinzaPersonal',
+        '--brancoPersonal',
+        '--verdePersonal',
+        '--laranjaPersonal',
+        '--vermelhoPersonal'
+    ]
+
+    const corAleatoriaVar = varCores[Math.floor(Math.random() * varCores.length)];
+    const corAleatoria = estilizacao.getPropertyValue(corAleatoriaVar).trim();
+
+    // Cria o card
+    const card = document.createElement('div');
+    card.classList.add('cardItem');
+    card.style.backgroundColor = corAleatoria;
+
+    card.innerHTML = `
+        <h1>Item: ${nome}</h1>
+        <h1>Peso: ${peso}</h1>
+    `;
+
+    listaItens.appendChild(card);
+
+    iptItem.value = '';
+    iptPeso.value = '';
+}
