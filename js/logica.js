de89a5e7835a5ec5a39db1b1ce2437b1470a88c2
Eduardo Lopes B. dos Santos <eduardolbs07@gmail.com>
2025-10-07 21:55:03 -0300

    Mudança Layout Distribuição Card

--- js/logica.js
+++ js/logica.js
@@ -4,7 +4,7 @@ const listaItens = document.getElementById('listaItens');
 
 function listaCard() {
     const nome = iptItem.value.trim();
-    const peso = iptPeso.value.trim();
+    const peso = Number(iptPeso.value.trim());
 
     // Evita adicionar cartões vazios
     if (nome === '' || peso === '') {
@@ -12,47 +12,43 @@ function listaCard() {
         return;
     }
 
+    if(peso < 0) {
+        alert('Por favor, digite um valor positivo')
+        return
+    }
+
     const estilizacao = getComputedStyle(document.documentElement);
-    const varCores = [
-        '--azulBrains',
-        '--laranjaBrains',
-        '--brancoBrains',
-        '--pretoBrains',
-        '--roxoBrains',
-        '--rosaBrains',
+    let corCondicao = '';
 
-        '--verdeClaroFamily',
-        '--azulClaroFamily',
-        '--brancoFamily',
-        '--cinzaFamily',
-        '--cinzaFamilEscuro',
-        '--amareloFamily',
-        '--verdeNeonFamily',
-
-        '--rosaClaroLovers',
-        '--lavandaLovers',
-        '--brancoLovers',
-        '--cinzaClaroLovers',
-        '--coralLovers',
-        '--turquesaLovers',
-        '--vinhoLovers',
-        '--roxoLovers',
-
-        '--azulPersonal',
-        '--cinzaPersonal',
-        '--brancoPersonal',
-        '--verdePersonal',
-        '--laranjaPersonal',
-        '--vermelhoPersonal'
-    ]
-
-    const corAleatoriaVar = varCores[Math.floor(Math.random() * varCores.length)];
-    const corAleatoria = estilizacao.getPropertyValue(corAleatoriaVar).trim();
+    if(peso >= 0 && peso <= 1) {
+        corCondicao = estilizacao.getPropertyValue('--verdeClaroFamily');
+    }
+    else if(peso >= 2 && peso <= 3) {
+        corCondicao = estilizacao.getPropertyValue('--verdeNeonFamily');
+    }
+    else if(peso >= 4 && peso <= 5) {
+        corCondicao = estilizacao.getPropertyValue('--azulClaroFamily');
+    }
+    else if(peso >= 6 && peso <= 7) {
+        corCondicao = estilizacao.getPropertyValue('--azulBrains');
+    }
+    else if(peso >= 8 && peso <= 9) {
+        corCondicao = estilizacao.getPropertyValue('--laranjaPersonal');
+    }
+    else if(peso >= 10 && peso <= 11) {
+        corCondicao = estilizacao.getPropertyValue('--laranjaBrains');
+    }
+    else if(peso >= 12 && peso <= 13) {
+        corCondicao = estilizacao.getPropertyValue('--coralLovers');
+    }
+    else if(peso >= 14) {
+        corCondicao = estilizacao.getPropertyValue('--vinhoLovers');
+    }
 
     // Cria o card
     const card = document.createElement('div');
     card.classList.add('cardItem');
-    card.style.backgroundColor = corAleatoria;
+    card.style.backgroundColor = corCondicao;
 
     card.innerHTML = `
         <h1>Item: ${nome}</h1>
